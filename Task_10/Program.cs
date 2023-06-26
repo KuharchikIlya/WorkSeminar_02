c8f75b54f7124b3cb80b26a480e5281a2a1bddf9
KuharchikIlya <svaps_srs@outlook.com>
2023-06-26 22:09:15 +0300

    add task in main

---
+++ Task_10/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var n = Math.Abs(Convert.ToInt64(Console.ReadLine()));
+
+        while (n >= 100)
+        {
+            n /= 10;
+        }
+
+        var r = n % 10;
+
+        Console.WriteLine(r);
+    }
+}
+
